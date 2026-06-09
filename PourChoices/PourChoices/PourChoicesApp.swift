43822cda9ff7b415b362e6ab905c8ddea4a711d4
lkartvedt <lindseykartvedt@gmail.com>
2026-06-08 22:56:15 -0700

    init

---
+++ PourChoices/PourChoices/PourChoicesApp.swift
@@ -0,0 +1,32 @@
+//
+//  PourChoicesApp.swift
+//  PourChoices
+//
+//  Created by Lindsey Kartvedt on 6/8/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PourChoicesApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
